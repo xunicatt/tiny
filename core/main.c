1449f52e6693aba38ebb63740e7aaaf25fde22fc
xunicatt <contact.aniket.biswas@gmail.com>
2026-07-15 19:05:29 +0530

    added: numbering to comments and force clock switch to master

--- core/main.c
+++ core/main.c
@@ -15,29 +15,46 @@ void main(void)
     sysclock_init();
     delay_init();
 
-    while (true)
+    while (true) {
         delay_ms(1000);
+    }
 }
 
 static void sysclock_init(void)
 {
-    // enable HSI,LSI and disable HSE
+    // A1. Enable HSI
     CLK->ICKR |= CLK_ICKR_HSIEN;
-    CLK->ICKR |= CLK_ICKR_LSIEN;
-    CLK->ECKR &= ~CLK_ECKR_HSEEN;
 
-    // wait for HSI to be stable
-    while (!(CLK->ICKR & CLK_ICKR_HSIRDY))
+    // A2. Wait for HSI to be stable
+    while ((CLK->ICKR & CLK_ICKR_HSIRDY) == 0) {
         nop();
+    }
 
-    // switch to highest clock frequency 16Mhz
+    // A3. Force the master clock to switch to HSI (0xE1)
+    CLK->SWR = 0xE1; 
+
+    // A4. Enable the clock switch
+    CLK->SWCR |= CLK_SWCR_SWEN; 
+
+    // A5. Wait until the clock switch is no longer busy
+    while ((CLK->SWCR & CLK_SWCR_SWBSY) != 0) {
+        nop();
+    }
+
+    // A6. Set clock div to 1 to run the Core at 16Mhz
     CLK->CKDIVR = 0x00;
 
-    // wait for LSI to be stable
+    // B1. Enable LSI
+    CLK->ICKR |= CLK_ICKR_LSIEN;
+
+    // B2. wait for LSI to be stable
     while (!(CLK->ICKR & CLK_ICKR_LSIRDY))
         nop();
 
-    // disable all peripherals
+    // C1. Disable HSE
+    CLK->ECKR &= ~CLK_ECKR_HSEEN;
+
+    // D1. Disable all peripherals
     CLK->PCKENR1 = 0x00;
     CLK->PCKENR2 = 0x00;
 }
