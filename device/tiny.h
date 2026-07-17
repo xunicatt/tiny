5924445bdc179c2db5648311b43505999fb220dd
xunicatt <contact.aniket.biswas@gmail.com>
2026-07-17 20:50:37 +0530

    removed: unnecessary complex gpio_cfg_t in device/tiny.h

--- device/tiny.h
+++ device/tiny.h
@@ -8,13 +8,14 @@
 
 #include <bsp/bsp.h>
 
-typedef struct {
-    gpio_t* port; 
-    gpio_pin_t pin;
-} gpio_cfg_t;
+// STATUS LED CONFIGS
+#define CONFIG_STATUS_LED_1_PORT GPIOD
+#define CONFIG_STATUS_LED_1_PIN  GPIO_PIN_4
 
-#define STATUS_LED1 ((gpio_cfg_t){.port = GPIOD, .pin = GPIO_PIN_4})
-#define STATUS_LED2 ((gpio_cfg_t){.port = GPIOD, .pin = GPIO_PIN_6})
-#define STATUS_LED3 ((gpio_cfg_t){.port = GPIOA, .pin = GPIO_PIN_3})
+#define CONFIG_STATUS_LED_2_PORT GPIOD
+#define CONFIG_STATUS_LED_2_PIN  GPIO_PIN_6
+
+#define CONFIG_STATUS_LED_3_PORT GPIOA
+#define CONFIG_STATUS_LED_3_PIN  GPIO_PIN_3
 
 #endif /* TINY_H */
